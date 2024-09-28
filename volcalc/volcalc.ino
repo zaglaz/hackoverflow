8f09356492ccc0a7dd2d200a1ec22125174e059d
zaglaz <96348494+zaglaz@users.noreply.github.com>
2024-09-28 17:00:01 -0400

    Added horizontalStepper function

--- volcalc/volcalc.ino
+++ volcalc/volcalc.ino
@@ -1,5 +1,9 @@
 #include <LiquidCrystal.h>
 #include <math.h>
+#include <Stepper.h>
+
+const int stepsPerRevolution = 2048;
+const int rpm = 15;
 
 const int trigPin = 9; 
 const int echoPin = 10;
@@ -7,20 +11,33 @@ float duration, distance;
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-
+Stepper horizontalScanner(stepsPerRevolution, 6, 7, 8, 13);
 //String volume = String(tan(M_PI * 45/180));
 
 void setup(){
-  //LCD screen
+  //Initialize stepper
+  horizontalScanner.setSpeed(rpm);
+  Serial.begin(9600);
+
+  //Initialize LCD screen
   lcd.begin(16, 2);
 
-  //Ultrasonic sensor
+  // Initialize ultrasonic sensor
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   Serial.begin(9600);
 }
 
 void loop(){
+  //Stepper movementp
+  Serial.println("clockwise");
+  horizontalScanner.step(stepsPerRevolution);
+  delay(500);
+
+  Serial.println("counter-clockwise");
+  horizontalScanner.step(-stepsPerRevolution);
+  delay(500);
+
   //LCD update
   lcd.setCursor(0, 1);
 
@@ -33,7 +50,8 @@ void loop(){
 
   //US sensor distance calculation and output
   duration = pulseIn(echoPin, HIGH);
-  distance = (duration*.0343)/2;
+  //cm -> meters
+  distance = ((duration*.0343)/2)/100;
   lcd.print(distance);
   delay(100);
 }
