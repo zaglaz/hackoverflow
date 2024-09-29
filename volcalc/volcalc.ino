42787825a16cc981860cd275d538b387f0417054
zaglaz <96348494+zaglaz@users.noreply.github.com>
2024-09-28 20:38:54 -0400

    Update volcalc.ino

--- volcalc/volcalc.ino
+++ volcalc/volcalc.ino
@@ -1,57 +1,52 @@
 #include <LiquidCrystal.h>
 #include <math.h>
-#include <Stepper.h>
 
-const int stepsPerRevolution = 2048;
-const int rpm = 15;
+#define TRIG_PIN 9
+#define ECHO_PIN 10
+#define DISTANCE_BOUND 30
 
-const int trigPin = 9; 
-const int echoPin = 10;
-float duration, distance;
-
+double duration, radius;
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-Stepper horizontalScanner(stepsPerRevolution, 6, 7, 8, 13);
-//String volume = String(tan(M_PI * 45/180));
 
-void setup(){
-  //Initialize stepper
-  horizontalScanner.setSpeed(rpm);
-  Serial.begin(9600);
-
-  //Initialize LCD screen
-  lcd.begin(16, 2);
-
-  // Initialize ultrasonic sensor
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  Serial.begin(9600);
+double getDistance(double duration) {
+  return (duration * 0.0343)/2;
 }
 
-void loop(){
-  //Stepper movementp
-  Serial.println("clockwise");
-  horizontalScanner.step(stepsPerRevolution);
-  delay(500);
+void setup() {
 
-  Serial.println("counter-clockwise");
-  horizontalScanner.step(-stepsPerRevolution);
-  delay(500);
+  //Initialize LCD screen/serial monitor
+  lcd.begin(16, 2);
+  Serial.begin(9600);
 
+  //Initialize ultrasonic sensor
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+
+  //Start serial monitor/LCD dialogue
+  Serial.print("VOLCALC");
+  lcd.print("Measuring...");
+  delay(2000);
+  lcd.clear();
+}
+
+void loop() {
   //LCD update
   lcd.setCursor(0, 1);
 
   //US sensor capture
-  digitalWrite(trigPin, LOW);
+  digitalWrite(TRIG_PIN, LOW);
   delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);  
+  digitalWrite(TRIG_PIN, LOW);
+  
+  duration = pulseIn(ECHO_PIN, HIGH);
+  radius = (DISTANCE_BOUND - getDistance(duration)) / 2;
+  Serial.println(getDistance(duration));
+  Serial.println(radius);
+  lcd.print("Radius: ");
+  lcd.print(radius);
 
-  //US sensor distance calculation and output
-  duration = pulseIn(echoPin, HIGH);
-  //cm -> meters
-  distance = ((duration*.0343)/2)/100;
-  lcd.print(distance);
-  delay(100);
+  delay(2000);
 }
